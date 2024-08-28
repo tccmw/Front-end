5285f4154bcee97e4fd68e5663330f7c40405c72
tccmw <jungmwoo6248@gmail.com>
2024-08-28 14:43:39 +0900

    apple

---
+++ apple/notice board/app/js(app)/background.js
@@ -0,0 +1,9 @@
+const images = ["1.jpg", "2.jpg", "3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `/JAVASCRIPT/app/img/${chosenImage}`;
+
+document.body.append(bgImage);
