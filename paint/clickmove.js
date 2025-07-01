377771b682883ea2951d56ecf11fce0d3192bfc9
정명우 <163850138+tccmw@users.noreply.github.com>
2025-07-01 11:39:29 +0900

    clickline

--- paint/clickmove.js
+++ paint/clickmove.js
@@ -15,74 +15,78 @@ ctx.lineWidth = lineWidth.value;
 let Mouse = false;
 let isFilling = false;
 
-function onMove(event){
-  if(Mouse){
-    ctx.lineTo(event.offsetX,event.offsetY);
+function onMove(event) {
+  // canvas의 위치를 기준으로 좌표를 계산
+  const rect = canvas.getBoundingClientRect(); // canvas의 상대적 위치를 얻음
+  const x = event.clientX - rect.left; // 마우스 X좌표에서 canvas의 왼쪽 경계값을 뺀 값
+  const y = event.clientY - rect.top; // 마우스 Y좌표에서 canvas의 상단 경계값을 뺀 값
+
+  if (Mouse) {
+    ctx.lineTo(x, y);
     ctx.stroke();
     return;
   }
-  ctx.moveTo(event.offsetX,event.offsetY);
+  ctx.moveTo(x, y);
 }
 
-function onMouseDown(){
+function onMouseDown() {
   Mouse = true;
 }
 
-function onMouseUp(){
+function onMouseUp() {
   Mouse = false;
   ctx.beginPath();
-} 
+}
 
-function lineWidthchage(event){
+function lineWidthchage(event) {
   ctx.lineWidth = event.target.value;
 }
 
-function onColorChange(event){
+function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
 
-function onColorClick(event){
+function onColorClick(event) {
   const colorvalue = event.target.dataset.color;
   ctx.strokeStyle = colorvalue;
   ctx.fillStyle = colorvalue;
   color.value = colorvalue;
 }
 
-function onFillClick(){
-  if(isFilling){
+function onFillClick() {
+  if (isFilling) {
     isFilling = false;
     fillBnt.innerText = "Fill";
-  }
-  else{
+  } else {
     isFilling = true;
     fillBnt.innerText = "Draw";
   }
 }
 
-function onClickFill(){
-  if(isFilling){
-    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+function onClickFill() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 }
 
-function onDestroyClick(event){
+function onDestroyClick(event) {
   ctx.fillStyle = "white";
-  ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-function onReactClick(){
+function onReactClick() {
   ctx.strokeStyle = "white";
   fillBnt.innerText = "Fill";
 }
 
-canvas.addEventListener("mousemove",onMove);
-canvas.addEventListener("mousedown",onMouseDown);
-canvas.addEventListener("mousedown",onClickFill);
-document.addEventListener("mouseup",onMouseUp);
-document.addEventListener("change",lineWidthchage);
-document.addEventListener("change",onColorChange);
-fillBnt.addEventListener("click",onFillClick);
-colorOption.forEach((color) => color.addEventListener("click",onColorClick));
-destroyBnt.addEventListener("click",onDestroyClick);
-raectBnt.addEventListener("click",onReactClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mousedown", onClickFill);
+document.addEventListener("mouseup", onMouseUp);
+document.addEventListener("change", lineWidthchage);
+document.addEventListener("change", onColorChange);
+fillBnt.addEventListener("click", onFillClick);
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
+destroyBnt.addEventListener("click", onDestroyClick);
+raectBnt.addEventListener("click", onReactClick);
