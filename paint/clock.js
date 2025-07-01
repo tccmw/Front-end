26f9e585df050139454eb7a8cf5bc607dc54f33e
정명우 <163850138+tccmw@users.noreply.github.com>
2025-07-01 11:40:28 +0900

    clock

---
+++ paint/clock.js
@@ -0,0 +1,22 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+getClock();
+setInterval(getClock, 1000);
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+clock.addEventListener("click", () => {
+  clock.style.color = getRandomColor();
+});
